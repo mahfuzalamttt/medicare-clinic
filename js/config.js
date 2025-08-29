f9a3a591c05d08a59d3e4dd7edf43b47628b2182
Md Mahfuz Alam <89962352+mahfuzalamttt@users.noreply.github.com>
2025-08-29 16:05:45 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDdirp8ldA2uD051zJPlR9DN8HUpaMmJBM",
+    authDomain: "clinic-management-system-dbdf2.firebaseapp.com",
+    projectId: "clinic-management-system-dbdf2",
+    storageBucket: "clinic-management-system-dbdf2.firebasestorage.app",
+    messagingSenderId: "305313116023",
+    appId: "1:305313116023:web:cc88b54d6472d7ec45bb72",
+    measurementId: "G-PQ78K0P4PQ"
+};
