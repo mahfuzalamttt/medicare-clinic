607e3437e786090c6c88526bc17ae63cbba1eade
Md Mahfuz Alam <89962352+mahfuzalamttt@users.noreply.github.com>
2025-09-01 01:36:50 +0530

    Delete onfig.js for secure Firebase keys

--- js/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDdirp8ldA2uD051zJPlR9DN8HUpaMmJBM",
-    authDomain: "clinic-management-system-dbdf2.firebaseapp.com",
-    projectId: "clinic-management-system-dbdf2",
-    storageBucket: "clinic-management-system-dbdf2.firebasestorage.app",
-    messagingSenderId: "305313116023",
-    appId: "1:305313116023:web:cc88b54d6472d7ec45bb72",
-    measurementId: "G-PQ78K0P4PQ"
-};
